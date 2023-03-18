69cf1c2bfe9eeb8955806640f9b1c341554b5901
huangshijie <huang_shi_jie@qq.com>
2023-03-18 17:37:23 +0800

    StringToBytes and BytesToString

--- gostring/string.go
+++ gostring/string.go
@@ -141,3 +141,18 @@ func RandStr(n int, letters string) string {
 	}
 	return *(*string)(unsafe.Pointer(&b))
 }
+
+// StringToBytes converts string to byte slice without a memory allocation.
+func StringToBytes(s string) []byte {
+	return *(*[]byte)(unsafe.Pointer(
+		&struct {
+			string
+			Cap int
+		}{s, len(s)},
+	))
+}
+
+// BytesToString converts byte slice to string without a memory allocation.
+func BytesToString(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
